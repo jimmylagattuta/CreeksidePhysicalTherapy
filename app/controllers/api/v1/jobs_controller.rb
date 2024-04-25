5409e44cbef46c942b3ddc7b336e0830ef3bfc97
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-04-25 11:31:36 -0700

    filtered out mislabled reviews, will look for facebook and yelp reviews

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -9,25 +9,20 @@ class Api::V1::JobsController < ApplicationController
 
   def pull_google_places_cache
     log_daily_visits
-    # puts "Fetching CSRF token..."
     csrf_token = form_authenticity_token
-    # puts "CSRF token fetched: #{csrf_token}"
-    # puts "Fetching cached Google Places reviews..."
     
-    # Parse the reviews string into an array of hashes
     reviews_json = GooglePlacesCached.cached_google_places_reviews
     reviews = JSON.parse(reviews_json)
     
-    # puts "Cached Google Places reviews fetched"
     if reviews.blank?
-      # puts "No reviews found, sending alert email..."
       OfficeMailer.alert_no_reviews_email.deliver_later
     else
       reviews.each do |review|
-        # puts "Name: #{review['author_name']}"
-        # puts "Rating: #{review['rating']}"
-        # puts "Text: #{review['text']}"
-        # puts "-------------------------"
+        # You can access filtered reviews here
+        puts "Name: #{review['author_name']}"
+        puts "Rating: #{review['rating']}"
+        puts "Text: #{review['text']}"
+        puts "-------------------------"
       end
     end
     render json: { reviews: reviews, csrf_token: csrf_token }
@@ -44,6 +39,7 @@ class Api::V1::JobsController < ApplicationController
     puts "Daily visits on #{today}: #{current_visits}"
     puts "*" * 100
   end
+
   def handle_unexpected_error(error)
     OfficeMailer.error_email("Unexpected Error", error.message).deliver_later
     render json: { error: "An unexpected error occurred." }, status: :internal_server_error
@@ -67,38 +63,30 @@ class GooglePlacesCached
   end
 
   def self.cached_google_places_reviews
-    # puts "Initializing Redis connection..."
     redis = Redis.new(url: ENV['REDIS_URL'])
-    # puts "Redis connection established"
 
-    # puts "Fetching cached data..."
     cached_data = redis.get('cached_google_places_reviews')
     reviews = JSON.parse(cached_data) if cached_data
 
     if cached_data.present?
-      # puts "Cached data found. Parsing..."
-      # Parse the JSON data into an array of hashes
       users = JSON.parse(cached_data)
 
-      # puts "Removing user with name 'Pdub ..'"
-      # Call the class method to remove the user with name "Pdub .."
-      remove_user_by_name(users, 'Pdub ..')
-      filtered_reviews = users.select { |review| review['rating'] == 5 }
+      reviews_from_oregon = users.select do |review|
+        review['user'] && review['user']['address_components'].any? do |component|
+          component['long_name'] == 'Oregon' && component['types'].include?('administrative_area_level_1')
+        end
+      end
 
-      # Convert the updated data back to a JSON string
-      updated_reviews = JSON.generate(filtered_reviews)
+      updated_reviews = JSON.generate(reviews_from_oregon)
       return updated_reviews
     end
 
-    # puts "No cached data found."
-    # puts "Fetching place IDs..."
     place_ids = ["ChIJT8nUWmzlBIgRnZluSKvaU7o", "ChIJy6GIldiP4okR-sQZEghTDSg", "ChIJt1CU6gxK0IkR4wEmOq3hYr4", "ChIJG5iRHpBhOIgRpqxsWqCW45o", "ChIJG4IRFhlSa4gRVOkjjz85dqE", "ChIJwS4aJMNCZIgRCnJm1UIi1DM", "ChIJf6kf46N1F4gRCtuy1-sFiFE", "ChIJPYnEYuIZxokROs5cvHzUe_A", "ChIJK3II3q_utYkR4B1voXTzigI", "ChIJkfcs-vB7ZIgRPIXGzSE94sQ", "ChIJKTOyIqC6ZIgRm_6vgwqt1C0", "ChIJUf1pNYumBIgRvqA-5r5JhWY", "ChIJ6UrV6tWpQYgRkhDQkPSseF4", "ChIJQ9SZk5oM3okRZhhzh4KmqV0", "ChIJccwssNidF4gRYkrpX72fqTA", "ChIJd6eSQJOZ9YgRdhGy82PWczE", "ChIJRwQnvhGFSk0RIN3V7ZxohCU", "ChIJZdP8j6G6ZIgRLowFu2-Fdco", "ChIJTfWBLxLSFogRjG83U7kxIpE", "ChIJZdP8j6G6ZIgRS0MV7drU2IE"]
 
     http = Net::HTTP.new("maps.googleapis.com", 443)
     http.use_ssl = true
     reviews = []
     place_ids.each do |place_id|
-      puts "Fetching Google Places! Details for place ID: #{place_id}"
       encoded_place_id = URI.encode_www_form_component(place_id)
       url = URI("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{encoded_place_id}&key=#{ENV['REACT_APP_GOOGLE_PLACES_API_KEY']}")
       request = Net::HTTP::Get.new(url)
@@ -107,7 +95,6 @@ class GooglePlacesCached
       parsed_response = JSON.parse(body)
 
       if parsed_response['status'] == 'OK'
-        # puts "Place details retrieved successfully"
         place_details = parsed_response['result']
         place_reviews = place_details.present? ? place_details['reviews'] || [] : []
         reviews.concat(place_reviews)
@@ -116,28 +103,25 @@ class GooglePlacesCached
       end
     end
 
-    # puts "Setting cached Google Places reviews..."
     redis.set("cached_google_places_reviews", JSON.generate(reviews))
     redis.expire("cached_google_places_reviews", 30.days.to_i)
     cached_reviews = redis.get("cached_google_places_reviews")
     reviews = JSON.parse(cached_reviews) if cached_reviews
 
     if cached_reviews.present?
-      # puts "Cached reviews found. Parsing..."
-      # Parse the JSON data into an array of hashes
       users = JSON.parse(cached_reviews)
 
-      # puts "Removing user with name 'Pdub ..'"
-      # Call the class method to remove the user with name "Pdub .."
-      remove_user_by_name(users, 'Pdub ..')
+      reviews_from_oregon = users.select do |review|
+        review['user'] && review['user']['address_components'].any? do |component|
+          component['long_name'] == 'Oregon' && component['types'].include?('administrative_area_level_1')
+        end
+      end
 
-      # Convert the updated data back to a JSON string
-      updated_reviews = JSON.generate(users)
+      updated_reviews = JSON.generate(reviews_from_oregon)
       return updated_reviews
     end
 
     puts "No cached reviews found."
     return { reviews: "No cached reviews" }
   end
-
 end
--- app/jobs/monthly_job.rb
+++ app/jobs/monthly_job.rb
@@ -6,7 +6,7 @@ class MonthlyJob
     require 'net/http'
     require 'json'
     require 'redis'
-    # Define your list of place IDs and locations
+
     places = ["ChIJT8nUWmzlBIgRnZluSKvaU7o", "ChIJy6GIldiP4okR-sQZEghTDSg", "ChIJt1CU6gxK0IkR4wEmOq3hYr4", "ChIJG5iRHpBhOIgRpqxsWqCW45o", "ChIJG4IRFhlSa4gRVOkjjz85dqE", "ChIJwS4aJMNCZIgRCnJm1UIi1DM", "ChIJf6kf46N1F4gRCtuy1-sFiFE", "ChIJPYnEYuIZxokROs5cvHzUe_A", "ChIJK3II3q_utYkR4B1voXTzigI", "ChIJkfcs-vB7ZIgRPIXGzSE94sQ", "ChIJKTOyIqC6ZIgRm_6vgwqt1C0", "ChIJUf1pNYumBIgRvqA-5r5JhWY", "ChIJ6UrV6tWpQYgRkhDQkPSseF4", "ChIJQ9SZk5oM3okRZhhzh4KmqV0", "ChIJccwssNidF4gRYkrpX72fqTA", "ChIJd6eSQJOZ9YgRdhGy82PWczE", "ChIJRwQnvhGFSk0RIN3V7ZxohCU", "ChIJZdP8j6G6ZIgRLowFu2-Fdco", "ChIJTfWBLxLSFogRjG83U7kxIpE", "ChIJZdP8j6G6ZIgRS0MV7drU2IE"]
     http = Net::HTTP.new("maps.googleapis.com", 443)
     http.use_ssl = true
@@ -15,79 +15,43 @@ class MonthlyJob
     places.each do |place|
       place_id = place
 
-      # Fetch place details from Google Places API
       encoded_place_id = URI.encode_www_form_component(place_id)
       url = URI("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{encoded_place_id}&key=#{ENV['REACT_APP_GOOGLE_PLACES_API_KEY']}")
       request = Net::HTTP::Get.new(url)
       response = http.request(request)
       body = response.read_body
       parsed_response = JSON.parse(body)
+
       if parsed_response['status'] == 'OK'
-        # puts "Successfully fetched place details for place ID: #{place_id}"
-        # puts "Parsed response:"
-        # puts parsed_response.inspect
         place_details = parsed_response['result']
-        # puts "Place details:"
-        # puts place_details.inspect
-        place_reviews = place_details.present? ? place_details['reviews'] || [] : []
-        # puts "Place reviews:"
-        # puts place_reviews.inspect
-        # puts "Merging reviews..."
-        # puts "Reviews before merging:"
-        # puts reviews.inspect
-        reviews.concat(place_reviews)
-        # puts "Reviews after merging:"
-        # puts reviews.inspect
+        address = place_details['formatted_address']
+
+        if address.include?('Oregon')
+          place_reviews = place_details.present? ? place_details['reviews'] || [] : []
+          reviews.concat(place_reviews)
+        else
+          puts "Skipping location #{place_id} because it's not in Oregon."
+        end
       else
         puts "Failed to retrieve place details for place ID: #{place_id}"
       end
     end
 
-    # Filter and process the reviews as needed
     filtered_reviews = []
     reviews.each do |review|
-      # puts "Applying review filtering logic..."
-      # You can apply filtering or processing logic here
       if review['rating'] == 5 && review['author_name'] != 'Pdub ..'
         filtered_reviews << review
       end
     end
 
-
-    require 'redis'
-
     redis = Redis.new(url: ENV['REDIS_URL'])
-    
-    begin
-      # puts redis.ping  # Test the connection
-    rescue => e
-      puts "Failed to connect to Redis: #{e.message}"
-    end
-    
-    
+
     if redis.exists('cached_google_places_reviews')
-      # puts "Cached reviews found. Clearing previous cache..."
       redis.del('cached_google_places_reviews')
-      # puts "Previous cache cleared."
     end
-    # puts "Caching filtered reviews..."
-    
 
-
-    
-    
-    begin
-      redis.set('cached_google_places_reviews', JSON.generate(filtered_reviews))
-      # puts "cached_google_places_reviews successful." # Added logging statement
-    rescue => e
-      puts "Error caching: #{e.message}" # Log the error message
-      # Optionally, you can re-raise the exception to propagate it further
-      # raise e
-    end
-    # puts "Filtered reviews cached successfully."
-    # puts "Setting expiration for cache..."
+    redis.set('cached_google_places_reviews', JSON.generate(filtered_reviews))
     redis.expire('cached_google_places_reviews', 30.days.to_i)
-    # puts "Cache expiration set successfully."
   rescue StandardError => e
     puts "Error in MonthlyJob: #{e.message}"
   end
