f3bf7efaad3c1ebf940cdd3fc77c15396d6e9f74
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-05-20 06:25:29 -0700

    about page update, ready for ssl and dns

--- client/src/pages/about/AppointmentInfo.js
+++ client/src/pages/about/AppointmentInfo.js
@@ -23,28 +23,6 @@ const AppointmentInfo = () => {
             <h3>FOLLOW UP PATIENTS</h3>
             <p>We ask that you please arrive five minutes early to your scheduled appointment time to complete the check-in process. We will always do our best to accommodate our patients; however, we cannot guarantee appointments outside of your scheduled appointment time. Please bring the items listed below to every appointment.</p>
           </section>
-
-          <section className="fees-payments">
-            <h3>Fees & Payments</h3>
-            <p>We make every effort to decrease the cost of your medical care. Therefore, we request payment arrangements for all office services at the time they are rendered unless prior arrangements have been made. We accept cash, checks, MasterCard, Visa, and Discover for your convenience. If we are a participating provider of your insurance company, we will bill them. However, payment is the patient’s responsibility. We will help in any way we can to assist you in handling claims.</p>
-          </section>
-
-          <section className="prescriptions-renewals">
-            <h3>Prescriptions & Renewals</h3>
-            <p>Effective October 6, 2014 the Drug Enforcement Agency has developed new mandates for narcotic medications.</p>
-            <ul>
-              <li>Narcotic medications cannot be filled or refilled over the phone, by fax, or electronically.</li>
-              <li>Pharmacies will only accept hand written prescriptions signed by your provider. Therefore, all prescriptions will need to picked up in the office during office hours.</li>
-              <li>Due to these new regulations, your provider will not be able to refill your narcotic medication(s) over the weekend.</li>
-              <li>Our providers are dedicated to your medical treatment and recovery. Therefore, it is very important to request all narcotic medication refills timely.</li>
-            </ul>
-            <p>Please ask a member of the clinical staff if you have any questions regarding this information.</p>
-            <p>Thank you in advance for your patience and understanding.</p>
-            <div>OFFICE HOURS FOR PRESCRIPTION REFILL REQUESTS</div>
-            <p>Monday–Thursday: 8:00am–4:30pm<br/>Friday: 8:00am–11:00am</p>
-            <div>OFFICE HOURS FOR PRESCRIPTION PICK UP</div>
-            <p>Monday–Friday: 8:00am–4:30pm</p>
-          </section>
         </div>
       </article>
     </div>
