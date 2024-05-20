c51c8de18dfd92c34e36f63c05c1ed81b1310fc8
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-05-20 12:02:50 -0700

    messing with reviews

--- client/src/components/CompanyReviewsPage.js
+++ client/src/components/CompanyReviewsPage.js
@@ -2,6 +2,23 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useCsrfToken } from './CsrfTokenContext';
 import './helpers/ReviewsHelpers.css';
 
+const shuffleArray = (array) => {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
 const CompanyReviewsPage = () => {
     const [reviews, setReviews] = useState([]);
     const [loading, setLoading] = useState(true);
