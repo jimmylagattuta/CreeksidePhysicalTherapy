ae7537e572a875fddd75e50a4d836388fb7f777e
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-05-30 05:04:40 -0700

    bugfixing redis

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::JobsController < ApplicationController
   rescue_from JSON::ParserError, with: :handle_json_parsing_error
 
   def index
-    # puts "Rendering index action..."
+    puts "1. Rendering index action..."
     render json: "Creekside Physical Therapy" * 1000
   end
 
@@ -17,10 +17,10 @@ class Api::V1::JobsController < ApplicationController
   
     cached_reviews = redis.get(cache_key)
     if cached_reviews
-      # puts "Using cached reviews"
+      puts "2. Using cached reviews"
       reviews = JSON.parse(cached_reviews)
     else
-      # puts "Fetching fresh reviews from Google Places API"
+      puts "3. Fetching fresh reviews from Google Places API"
       reviews = GooglePlacesCached.fetch_five_star_reviews_for_companies
       redis.setex(cache_key, 7.days.to_i, reviews.to_json)
     end
@@ -28,22 +28,21 @@ class Api::V1::JobsController < ApplicationController
     creekside_reviews = reviews["Creekside Physical Therapy"] || []
     northwest_reviews = reviews["Northwest Extremity Specialists"] || []
   
-    # puts "Successfully fetched reviews for Creekside and Northwest"
+    puts "4. Successfully fetched reviews for Creekside and Northwest"
     csrf_token = form_authenticity_token
     render json: { creekside_reviews: creekside_reviews, northwest_reviews: northwest_reviews, csrf_token: csrf_token }
   end
-  
 
   private
 
   def handle_unexpected_error(error)
-    # puts "Handling unexpected error: #{error.message}"
+    puts "5. Handling unexpected error: #{error.message}"
     OfficeMailer.error_email("Unexpected Error", error.message).deliver_later
     render json: { error: "An unexpected error occurred: #{error.message}" }, status: :internal_server_error
   end
 
   def handle_json_parsing_error(error)
-    # puts "Handling JSON parsing error: #{error.message}"
+    puts "6. Handling JSON parsing error: #{error.message}"
     error_message = "Failed to parse JSON: #{error.message}"
     OfficeMailer.error_email("JSON Parsing Error", error_message).deliver_later
     render json: { error: error_message }, status: :unprocessable_entity
@@ -58,7 +57,7 @@ class GooglePlacesCached
   require 'openssl'
 
   def self.fetch_five_star_reviews_for_companies
-    # puts "Fetching five-star reviews for companies..."
+    puts "7. Fetching five-star reviews for companies..."
     companies = {
       "Creekside Physical Therapy" => ["ChIJT8nUWmzlBIgRnZluSKvaU7o", "ChIJy6GIldiP4okR-sQZEghTDSg", "ChIJt1CU6gxK0IkR4wEmOq3hYr4", "ChIJ4ehfGL8JlVQR3QwufLzN5SI", "ChIJ_U-CjPEMlVQRLymo_u5om1o", "ChIJI7hPTTYNlVQRmK-_GuttTHs"],
       "Northwest Extremity Specialists" => ["ChIJf07ARPkJlVQRJCA-9wte444", "ChIJi3RsjPEMlVQRt1cOeU3_g48", "ChIJSRSts-CglVQRfXCyBEPzHNg"]
@@ -72,10 +71,10 @@ class GooglePlacesCached
 
     cached_reviews = redis.get(cache_key)
     if cached_reviews
-      puts "Using cached reviews"
+      puts "8. Using cached reviews"
       reviews = JSON.parse(cached_reviews)
     else
-      puts "Fetching fresh reviews from Google Places API"
+      puts "9. Fetching fresh reviews from Google Places API"
       reviews = fetch_reviews_from_google(companies, api_key)
       redis.setex(cache_key, 30.days.to_i, reviews.to_json)
     end
@@ -88,16 +87,16 @@ class GooglePlacesCached
     redis = Redis.new(url: ENV["REDIS_URL"])
 
     companies.each do |company, place_ids|
-      puts "Fetching reviews for company: #{company}"
+      puts "10. Fetching reviews for company: #{company}"
       reviews[company] = place_ids.flat_map do |place_id|
-        puts "Fetching reviews for place ID: #{place_id}"
+        puts "11. Fetching reviews for place ID: #{place_id}"
         review_key = "reviews:#{company}:#{place_id}"
         review_details = redis.get(review_key)
         
         if review_details
-          puts "Review details for place ID #{place_id}: #{review_details}"
+          puts "12. Review details for place ID #{place_id}: #{review_details}"
         else
-          puts "No reviews found for place ID #{place_id}"
+          puts "13. No reviews found for place ID #{place_id}"
         end
         
         # Fetch fresh reviews
@@ -105,21 +104,20 @@ class GooglePlacesCached
         
         # Clear existing reviews in Redis
         redis.del(review_key)
-        puts "Deleted review key: #{review_key}"
+        puts "14. Deleted review key: #{review_key}"
         
         fresh_reviews
       end
-      puts "Successfully fetched and updated reviews for company: #{company}"
+      puts "15. Successfully fetched and updated reviews for company: #{company}"
     end
 
-    # puts "Finished fetching reviews for all companies"
     reviews
   end
 
   private
 
   def self.fetch_five_star_reviews_for_place_id(place_id, api_key)
-    # puts "Fetching five-star reviews for place ID: #{place_id}"
+    puts "16. Fetching five-star reviews for place ID: #{place_id}"
     http = Net::HTTP.new("maps.googleapis.com", 443)
     http.use_ssl = true
     url = URI("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{place_id}&fields=reviews&key=#{api_key}")
@@ -128,17 +126,16 @@ class GooglePlacesCached
     data = JSON.parse(response.body)
 
     if data['status'] == 'OK'
-      # puts "Successfully fetched reviews for place ID: #{place_id}"
+      puts "17. Successfully fetched reviews for place ID: #{place_id}"
       reviews = data['result']['reviews'] || []
       reviews.select { |review| review['rating'] == 5 }.each do |review|
-        # puts " - #{review['author_name']}: #{review['text']}"
+        puts "18. - #{review['author_name']}: #{review['text']}"
       end
       reviews.select { |review| review['rating'] == 5 }
     else
-      # puts "Error fetching reviews for Place ID #{place_id}: #{data['status']}"
+      puts "19. Error fetching reviews for Place ID #{place_id}: #{data['status']}"
       Rails.logger.error("Error fetching reviews for Place ID #{place_id}: #{data['status']}")
       []
     end
   end
 end
-
